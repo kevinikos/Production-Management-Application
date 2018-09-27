aae19a9e6148ead111dc747a4021cbfd194e7a19
mrpapaj <kevin.nikolas123@gmail.com>
2018-09-27 12:17:17 +0200

    Zmiana wizualna panelu logowania z uzyciem pakietu (materialDesign)

--- manage_app/manage_app/Panel.xaml.cs
+++ manage_app/manage_app/Panel.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,5 +25,34 @@ namespace manage_app
         {
             InitializeComponent();
         }
+
+        private void txtPokazDane_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=LAPTOP-OIOAR14S\MYSQL2017; Initial Catalog=logowanie; User ID=sa; Password=whatever2424");
+            DataTable datatable = new DataTable();
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                    MessageBox.Show("Połączono z bazą dancyh");
+                }
+
+                SqlDataAdapter sqlDA = new SqlDataAdapter("SELECT * FROM Uzytkownik", sqlCon);
+                sqlDA.Fill(datatable);
+                dg1.ItemsSource = datatable.DefaultView;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+
+        }
+
     }
 }
